3b3c9c60935cb5c8836eb32d33e5150994c2ef04
故忘 <15730982771@163.com>
2019-08-30 20:45:09 +0800

    update bug

---
+++ app/src/main/java/com/sincerity/sinceutils/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.sincerity.sinceutils;
+
+import android.app.Application;
+
+/**
+ * Created by Sincerity on 2019/8/30.
+ * 描述：
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
